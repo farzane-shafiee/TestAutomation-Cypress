3e7895ce8eea111243211b3a8fb015d5b1cee945
Farzaneh Shafiee <F.Shafiee@FlyToday.ir>
2022-09-21 17:24:31 +0430

    insert sorting flight

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,7 +1,7 @@
 /// <reference type = "cypress"/>
 
 describe('Suite 1', function (){
-  it.only('flytoday', function () {
+  it('flytoday', function () {
     cy.visit("https://betademo.flytoday.ir").wait(2000)
 
     cy.get('button[data-test="originSelectBox"]').last().click({force: true})
@@ -14,7 +14,8 @@ describe('Suite 1', function (){
         .children('button[data-test="datePickerDay"]').click({force: true})
     cy.contains('تایید').click({force: true})
     cy.get('button[data-test="flightSearchBtn"]').last().click({force: true})
-    cy.wait(25000)
+    cy.wait(23000)
+    // filtering
     cy.get('[title="ایران ایر تور"][type=checkbox]').check({force: true}).wait(2000)
     cy.get('svg[class*=visible]').then(el => {
       expect(el).to.be.visible
@@ -27,6 +28,15 @@ describe('Suite 1', function (){
     })
     cy.get('div[data-test="list-filter-dialog"]').should('have.length', 1)
     cy.get('div[data-test="show-filter-dialog"]').should('have.text', 'ایران ایر تور')
+
+  //  sorting
+    cy.get('[class*=green-offer-color]').then(cheapest_expected_result => {
+      const expected_result = cheapest_expected_result.text().substring(0,1)
+      cy.get('[class="text-nowrap"]').contains('ارزان‌ترین').siblings('span').then(cheapest_actual_result => {
+        const actual_result = cheapest_actual_result.text().substring(0,1)
+        expect(expected_result).equals(actual_result)
+      })
+    })
   })
 
   it('should pass api login', function () {
@@ -36,4 +46,11 @@ describe('Suite 1', function (){
           expect(data.status).equals(200)
         })
   });
+
 })
+it.only('should ', function () {
+  fetch('https://apidemo.flytoday.ir/User/CheckLogin').then((resp) => resp.json())
+      .then((data) => {
+        console.log("log is : " + data)
+      })
+});
